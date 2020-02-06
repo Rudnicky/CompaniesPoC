615891271c67e19f783b04c854f02515024fe6f5
Pawel Rudnicki <prudnicki@pdisoftware.com>
2020-02-06 12:38:33 +0100

    Added swagger

--- CompaniesPoC/Startup.cs
+++ CompaniesPoC/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace CompaniesPoC
 {
@@ -40,11 +41,28 @@ namespace CompaniesPoC
             services.AddAutoMapper(typeof(Core.Mappers.MapperConfiguration));
 
             services.AddControllers();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Companies API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Companies API V1");
+            });
+
             // global cors policy
             app.UseCors(x => x
                 .AllowAnyOrigin()
